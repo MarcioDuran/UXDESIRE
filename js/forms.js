b7db701469ba4685af61f3d5040ecfc9000916c1
Marcio  Duran <marcioduran@gmail.com>
2023-07-29 16:06:44 -0300

    Teste de Validação de Formulário de Email
    Teste de campo formulário de Email, para gmail.

--- js/forms.js
+++ js/forms.js
@@ -11,7 +11,7 @@
 					successCl:'success',
 					successShow:'4000',
 					mailHandlerURL:'bat/MailHandler.php',
-					ownerEmail:'support@template-help.com',
+					ownerEmail:'marcioduran@gmail.com',
 					stripHTML:true,
 					smtpMailServer:'localhost',
 					targets:'input,textarea',
