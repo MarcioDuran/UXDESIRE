330ea93c9db07ab0de0ca4c1078b15a25256a1b7
Marcio  Duran <marcioduran@gmail.com>
2023-08-22 15:54:16 -0300

    Refactory Codigo
    Reutilização de Spcript

--- js/tradutor.js
+++ js/tradutor.js
@@ -1,30 +1,19 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    const languageSelect = document.getElementById('language-select');
-    const applyLanguageButton = document.getElementById('apply-language');
+// Adiciona um botão para traduzir o texto
+const translateButton = document.createElement("button");
+translateButton.textContent = "Traduzir";
+translateButton.addEventListener("click", function() {
+  // Obtém o texto a ser traduzido
+  const text = document.querySelector(".text").textContent;
 
-    applyLanguageButton.addEventListener('click', () => {
-        const selectedLanguage = languageSelect.value;
-        translatePage(selectedLanguage);
-    });
+  // Obtém o idioma atual
+  const language = document.querySelector(".language").value;
 
-    function translatePage(language) {
-        // Implement your translation logic here
-        // For demonstration purposes, I'm just changing the footer copyright text
-        const copyrightText = document.querySelector('.copyright-text p');
+  // Obtém a tradução do texto
+  const translatedText = dictionary[language][text];
 
-        switch (language) {
-            case 'en':
-                copyrightText.innerHTML = "<br>Copyright &copy; 2023 <br>NFT Marcio Duran spirit of imagination @UxDesire<br><br>Design: <a rel='nofollow' href='https://opensea.io/Marcio-Duran' target='_parent'>Go to OpenSean MarketPlace Marcio Duran</a></p>";
-                break;
-            case 'pt':
-                copyrightText.innerHTML = "<br>Direitos autorais &copy; 2023 <br>NFT Marcio Duran espírito de imaginação @UxDesire<br><br>Design: <a rel='nofollow' href='https://opensea.io/Marcio-Duran' target='_parent'>Ir para OpenSean MarketPlace Marcio Duran</a></p>";
-                break;
-            case 'es':
-                copyrightText.innerHTML = "<br>Derechos de autor &copy; 2023 <br>NFT Marcio Duran espíritu de imaginación @UxDesire<br><br>Design: <a rel='nofollow' href='https://opensea.io/Marcio-Duran' target='_parent'>Ir a OpenSean MarketPlace Marcio Duran</a></p>";
-                break;
-            default:
-                break;
-        }
-    }
+  // Atualiza o texto com a tradução
+  document.querySelector(".text").textContent = translatedText;
 });
+
+// Adiciona o botão ao documento
+document.querySelector(".container").appendChild(translateButton);
