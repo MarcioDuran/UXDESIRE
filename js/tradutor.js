dee1f2ea473729b0fb58e313de63e7e4da1313c0
Marcio  Duran <marcioduran@gmail.com>
2023-08-23 13:11:34 -0300

    TESTE COM IA
    REFACTORY CODIGO

--- js/tradutor.js
+++ js/tradutor.js
@@ -1,35 +1,30 @@
-// Importa a API do Google Translate
-<script src="https://translate.googleapis.com/translate_a/element.js?hl=pt-BR&sl=pt-BR&tl=en"></script>
-
-/// Seletor de idiomas
+// Seletor de elementos
 const text = document.querySelector(".text");
 const language = document.querySelector(".language");
 const translateButton = document.querySelector(".tradutor");
 
 // Função para traduzir o texto
-function translateText(language) {
-  // Obtém o texto a ser traduzido
+function translateText(selectedLanguage) {
   const textToTranslate = text.textContent;
 
-  // Realiza a tradução do texto
   const translatedText = googleTranslate.translate(textToTranslate, {
     from: "pt-BR",
-    to: language,
+    to: selectedLanguage,
   });
 
-  // Atualiza o texto com a tradução
   text.textContent = translatedText;
 }
 
-// Eventos
-translateButton.addEventListener("click", function() {
-  // Obtém o idioma selecionado
-  const selectedLanguage = language.value;
+// Inicializa a API do Google Translate
+const googleTranslate = new google.translate.TranslateElement({}, 'google-translate-element');
+googleTranslate.showBanner(false);
 
-  // Realiza a tradução do texto
+// Evento de clique para o botão de tradução
+translateButton.addEventListener("click", function() {
+  const selectedLanguage = language.value;
   translateText(selectedLanguage);
 });
 
-// Inicializa o idioma
+// Inicializa o idioma padrão
 language.value = "en";
 translateText(language.value);
