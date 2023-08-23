1d913fae9d47f9e91ae4ed03bcb7d7659b5e889f
Marcio  Duran <marcioduran@gmail.com>
2023-08-23 15:28:22 -0300

    UPDATE
    REDESIGNER

--- js/tradutor.js
+++ js/tradutor.js
@@ -3,17 +3,18 @@ const text = document.querySelector(".text");
 const language = document.querySelector(".language");
 const translateButton = document.querySelector(".tradutor");
 
+// Inicializa a API do Google Translate
+const googleTranslate = new google.translate.TranslateElement({}, 'google-translate-element');
+googleTranslate.showBanner(false);
+
 // Função para traduzir o texto
 function translateText(selectedLanguage) {
   // Obtém o texto a ser traduzido
   const textToTranslate = text.textContent;
 
-  // Inicializa a API do Google Translate
-  const googleTranslate = new google.translate.TranslateElement({}, 'google-translate-element');
-  googleTranslate.showBanner(false);
-
   // Realiza a tradução do texto
   googleTranslate.translate(textToTranslate, "pt-BR", selectedLanguage, function(result) {
+    // Atualiza o texto com a tradução
     text.textContent = result.translation;
   });
 }
